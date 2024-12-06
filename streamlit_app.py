8799245a93edd077ee380898aeafaced52ac9565
rjcontrerasr <156374431+rjcontrerasr@users.noreply.github.com>
2024-12-06 17:47:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
+import pandas as pd
+
+# Add a text input field for the GitHub raw URL
+url = st.text_input("Enter the GitHub raw URL of the CSV file:", 
+                    "https://raw.githubusercontent.com/JeanJMH/Financial_Classification/main/Classification_data.csv")
+
+# Load the dataset if a valid URL is provided
+if url:
+    try:
+        df = pd.read_csv(url)
+        st.write("CSV Data:")
+        st.write(df)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write(df)
